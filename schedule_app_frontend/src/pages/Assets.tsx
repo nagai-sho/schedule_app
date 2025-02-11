6b09cb9cea7a40d737ff4dae4bc2f80ac0d5ea45
nagai-sho <as.uland.dr@gmail.com>
2025-02-11 23:00:51 +0900

    Assets.tsxの編集

--- schedule_app_frontend/src/pages/Assets.tsx
+++ schedule_app_frontend/src/pages/Assets.tsx
@@ -9,56 +9,90 @@ export const Assets: FC = () => {
 
   return (
     <>
+      <Header />
       <div>
-        <Header />
-
-        <p>{id}</p>
-        <h2>編集画面</h2>
-
-        <label htmlFor="lastName">last_name: </label>
-        <input type="text" name="lastName" id="lastName" placeholder="name" />
-        <br />
-        <label htmlFor="firstName">first_name: </label>
-        <input type="text" name="firstName" id="firstName" placeholder="name" />
-        <br />
-        <label htmlFor="nameKana">name_kana: </label>
-        <input type="text" name="nameKana" id="nameKana" placeholder="name" />
-        <br />
-        <label htmlFor="age">age: </label>
-        <input type="number" name="age" id="age" placeholder="age" />
-        <br />
-        <label htmlFor="phoneNumber">phone_number: </label>
-        <input
-          type="string"
-          name="phoneNumber"
-          id="phoneNumber"
-          placeholder="phone"
-        />
-        <br />
-        <label htmlFor="address">address: </label>
-        <input
-          type="string"
-          name="address"
-          id="address"
-          placeholder="address"
-        />
-        <br />
-        <Button
-          type="submit"
-          onClick={() => {
-            console.log('save!');
-          }}
+        <p>tab表示、クリックで表示内容切り替え</p>
+        <div
+          className="select_tab"
+          style={{ display: 'flex', justifyContent: 'center' }}
         >
-          保存
-        </Button>
-        <br />
-      </div>
-      <div>
-        <div>登録社員一覧</div>
-        <div>（indexより抽出・表示）</div>
-        <div>（indexより抽出・表示）</div>
-        <div>（indexより抽出・表示）</div>
-        <div>クリックで編集・削除ができるように</div>
+          <p>作業員 /</p>
+          <p>現場情報 /</p>
+          <p>車両</p>
+        </div>
+
+        <div className="main" style={{ display: 'flex' }}>
+          <div className="operation">
+            <h2>編集画面</h2>
+            <label htmlFor="lastName">last_name: </label>
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              placeholder="name"
+            />
+            <br />
+            <label htmlFor="firstName">first_name: </label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              placeholder="name"
+            />
+            <br />
+            <label htmlFor="nameKana">name_kana: </label>
+            <input
+              type="text"
+              name="nameKana"
+              id="nameKana"
+              placeholder="name"
+            />
+            <br />
+            <label htmlFor="age">age: </label>
+            <input type="number" name="age" id="age" placeholder="age" />
+            <br />
+            <label htmlFor="phoneNumber">phone_number: </label>
+            <input
+              type="string"
+              name="phoneNumber"
+              id="phoneNumber"
+              placeholder="phone"
+            />
+            <br />
+            <label htmlFor="address">address: </label>
+            <input
+              type="string"
+              name="address"
+              id="address"
+              placeholder="address"
+            />
+            <br />
+            <label htmlFor="affiliated">company: </label>
+            <input
+              type="string"
+              name="affiliated"
+              id="affiliated"
+              placeholder="affiliated company"
+            />
+            <br />
+            <Button
+              type="submit"
+              onClick={() => {
+                console.log('save!');
+              }}
+            >
+              保存
+            </Button>
+            <br />
+          </div>
+          <div className="">
+            <h2>登録社員一覧</h2>
+            <div>（indexより抽出・表示）</div>
+            <div>（indexより抽出・表示）</div>
+            <div>（indexより抽出・表示）</div>
+            <div>クリックで編集・削除ができるように</div>
+          </div>
+        </div>
       </div>
       <Link to="/">Topへ</Link>
     </>
