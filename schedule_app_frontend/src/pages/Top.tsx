04d0710c47bb83f644db0a5d12d9b50582a47f00
nagai-sho <as.uland.dr@gmail.com>
2025-02-11 22:15:10 +0900

    Clock.tsxコンポーネントの作成

--- schedule_app_frontend/src/pages/Top.tsx
+++ schedule_app_frontend/src/pages/Top.tsx
@@ -1,34 +1,21 @@
 import { useState } from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
-import '../styles/Top.css';
+import '../styles/top.css';
 import StopWatch from '../components/StopWatch';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Clock from '../components/Clock';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1> */}
       <Header />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Clock />
       <StopWatch />
       <div>
-        <h2>登録・編集</h2>
         <Link to="/assets/edit">登録・編集へ</Link>
         <br />
         <Link to="/calender/show">カレンダーへ</Link>
