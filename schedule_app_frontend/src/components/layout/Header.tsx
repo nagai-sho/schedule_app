8c2488b330d75466b3a9012837a476dda360e05e
nagai-sho <as.uland.dr@gmail.com>
2025-02-22 17:55:44 +0900

    Added a setting to fix the top of the header.

--- schedule_app_frontend/src/components/layout/Header.tsx
+++ schedule_app_frontend/src/components/layout/Header.tsx
@@ -1,5 +1,3 @@
-// import ReactDom from 'react-dom';
-// import '../../styles/header.css';
 import { use, useState } from 'react';
 import {
   AppBar,
@@ -15,7 +13,7 @@ import {
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import { Link } from 'react-router-dom';
-// import Logo from '../../assets/images/logo.png';
+import logo from '../../assets/images/kyowasetsubi_logo.png';
 
 const setNavLinks: Array<{ text: string; url: string }> = [
   { text: 'Top', url: '/' },
@@ -35,75 +33,82 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header>
-      <AppBar component="header" position="static">
-        <Container maxWidth="md">
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Box
-              sx={{
-                display: 'flex',
-                textDecoration: 'none',
-                color: 'text.primary',
-              }}
+    <AppBar component="header" position="fixed" id="header">
+      <Container
+        maxWidth={false}
+        sx={{
+          minHeight: '100px',
+          maxHeight: '200px',
+          maxWidth: { xs: '100%', md: '100%', lg: 'none' },
+        }}
+      >
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              textDecoration: 'none',
+            }}
+          >
+            <Link to="/calender/show">
+              <img src={logo} alt="Calender-Signage" height="50" width="auto" />
+            </Link>
+            <Typography sx={{ fontSize: '25px' }}>株式会社京和設備</Typography>
+          </Box>
+          <Box>
+            <List
+              component="nav"
+              sx={{ display: 'flex', justifyContent: 'flex-start' }}
             >
-              <Typography component="h1">
-                <Link to="calender/show">
-                  <img
-                    src="#"
-                    alt="Calender-Signage"
-                    height="60"
-                    width="auto"
-                  />
-                </Link>
-              </Typography>
-              <Typography component="h1">
-                {/* <Link to="calender/show"> */}
-                <h2>株式会社京和設備</h2>
-                {/* </Link> */}
-              </Typography>
-            </Box>
-            <Box>
-              <List
-                component="nav"
-                sx={{ display: 'flex', justifyContent: 'flex-start' }}
-              >
+              <ListItem disablePadding>
+                <ListItemButton
+                  sx={{
+                    textAlign: 'center',
+                    display: { xs: 'block', md: 'none' },
+                  }}
+                  onClick={handleDrawerOpen}
+                >
+                  <ListItemText primary={<MenuIcon />} />
+                </ListItemButton>
+              </ListItem>
+              {setNavLinks.map((navLink) => (
                 <ListItem disablePadding>
                   <ListItemButton
                     sx={{
                       textAlign: 'center',
-                      display: { xs: 'block', md: 'none' },
+                      display: { xs: 'none', md: 'block' },
                     }}
-                    onClick={handleDrawerOpen}
+                    component={Link}
+                    to={navLink.url}
                   >
-                    <ListItemText primary={<MenuIcon />} />
+                    <ListItemText primary={navLink.text} />
                   </ListItemButton>
                 </ListItem>
-                {setNavLinks.map((navLink) => (
-                  <ListItem disablePadding>
-                    <ListItemButton
-                      sx={{
-                        textAlign: 'center',
-                        display: { xs: 'none', md: 'block' },
-                      }}
-                      component={Link}
-                      to={navLink.url}
-                    >
-                      <ListItemText primary={navLink.text} />
-                    </ListItemButton>
-                  </ListItem>
-                ))}
-              </List>
-            </Box>
-            <Drawer
-              anchor="right"
-              open={open}
-              onClose={handleDrawerClose}
-              PaperProps={{ style: { width: '100%' } }}
+              ))}
+            </List>
+          </Box>
+          <Drawer
+            anchor="right"
+            open={open}
+            onClose={handleDrawerClose}
+            PaperProps={{ style: { width: '100%' } }}
+          >
+            <List
+              component="nav"
+              sx={{ display: 'block', justifyContent: 'normal' }}
             >
-              <List
-                component="nav"
-                sx={{ display: 'block', justifyContent: 'normal' }}
-              >
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={handleDrawerClose}
+                  sx={{
+                    textAlign: 'center',
+                    borderBottom: 'solid 1px #696969',
+                  }}
+                >
+                  <ListItemText primary={<CloseIcon />} />
+                </ListItemButton>
+              </ListItem>
+              {setNavLinks.map((navLink) => (
                 <ListItem disablePadding>
                   <ListItemButton
                     onClick={handleDrawerClose}
@@ -111,31 +116,18 @@ const Header: React.FC = () => {
                       textAlign: 'center',
                       borderBottom: 'solid 1px #696969',
                     }}
+                    component={Link}
+                    to={navLink.url}
                   >
-                    <ListItemText primary={<CloseIcon />} />
+                    <ListItemText primary={navLink.text} />
                   </ListItemButton>
                 </ListItem>
-                {setNavLinks.map((navLink) => (
-                  <ListItem disablePadding>
-                    <ListItemButton
-                      onClick={handleDrawerClose}
-                      sx={{
-                        textAlign: 'center',
-                        borderBottom: 'solid 1px #696969',
-                      }}
-                      component={Link}
-                      to={navLink.url}
-                    >
-                      <ListItemText primary={navLink.text} />
-                    </ListItemButton>
-                  </ListItem>
-                ))}
-              </List>
-            </Drawer>
-          </Box>
-        </Container>
-      </AppBar>
-    </header>
+              ))}
+            </List>
+          </Drawer>
+        </Box>
+      </Container>
+    </AppBar>
   );
 };
 
