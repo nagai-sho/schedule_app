a0be9283bcff80af56728ee3e41c0b8398311f29
nagai-sho <as.uland.dr@gmail.com>
2025-04-17 00:18:05 +0900

    Add json file

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Get all users successfully.' })
   async findAll() {
     return this.userService.findAll();
   }
